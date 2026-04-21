7c1ac7addbc380b379cf13302d5fad88eaff5035
Hilal Erdogan Sumnu <hilalsumnu@gmail.com>
2026-04-21 18:57:19 +0300

    chore(50327): buildNumber bump
    50326 TestFlight'a çıktı. Bu build Apple private-relay
    e-postası için "E-postamı Gizle" fix'ini taşıyor (commit 4f5f20c).

--- app.config.ts
+++ app.config.ts
@@ -55,7 +55,7 @@ const config: ExpoConfig = {
   ios: {
     supportsTablet: true,
     bundleIdentifier: env.iosBundleId,
-    buildNumber: "50326",
+    buildNumber: "50327",
     usesAppleSignIn: true,
     privacyManifests: {
       NSPrivacyTracking: false,
